0db6dc38504e9f8940ec4fea0ae2291c0f9acabe
xatkumaulank <70027310+xatkumaulank@users.noreply.github.com>
2021-09-09 10:44:43 +0700

    high-order function

--- src/main/kotlin/High_Order_Function.kt
+++ src/main/kotlin/High_Order_Function.kt
@@ -1,6 +1,34 @@
 
 
+//I. High-order function là function mà lấy một function khác làm tham số, hoặc return về một function
+fun exampleHighOrderFunction(){
+    val items = listOf(1, 2, 3, 4, 5)
+    items.fold(0) { acc: Int, i: Int ->
+        print("acc = $acc , i = $i")
+        val result = acc + i
+        println(" result = $result")
+        result
+    }
 
+    val joinedToString = items.fold("Elements:") { acc, i -> "$acc $i" }
+    println(joinedToString)
+}
+fun exampleHighOrderFunction2(name : String, helloFunction: (String) -> Unit){
+    print("Xin chào ")
+    helloFunction(name)
+}
+fun returnAddFunction() : ((Int,Int) -> Int){
+    return ::addFunction
+}
+fun addFunction(a : Int, b : Int) : Int{
+    return a + b
+}
+var abc = fun(s: String): Int { return s.toIntOrNull() ?: 0 }
 fun main(){
-
+    println(abc.invoke("10"))
+    exampleHighOrderFunction()
+    exampleHighOrderFunction2("Hòa") { s: String -> println("$s đẹp trai") }
+    println(returnAddFunction().invoke(1,6))
+    val a = {i:Int -> i+1}
+    println(a.invoke(15))
 }
